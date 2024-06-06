205c2eb25c0ee66afbe06a449aa31554c3beb814
m-pasima <theivykierachronicles@gmail.com>
2024-06-06 02:37:14 +0100

    Update main.tf with ecs-cluster module

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -54,3 +54,27 @@ module "application_load_balancer" {
   vpc_id                 = module.vpc.vpc_id
   arn_certificate        = module.acm.arn_certificate
 }
+
+module "ecs" {
+  source                          = "./modules/ecs-cluster"
+  project_name                    = var.project_name
+  execution_role_arn              = module.ecs-task-execution-role.ecs_tasks_execution_role_arn
+  network_mode                    = "awsvpc"
+  requires_compatibilities        = ["FARGATE"]
+  cpu                             = "256"
+  memory                          = "512"
+  container_image                 = "amazon/amazon-ecs-sample"
+  container_port                  = 80
+  region                          = var.region
+  launch_type                     = "FARGATE"
+  platform_version                = "LATEST"
+  desired_count                   = 1
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent      = 200
+  enable_ecs_managed_tags         = true
+  propagate_tags                  = "SERVICE"
+  subnets                         = [module.vpc.public_subnet_az1_id, module.vpc.public_subnet_az2_id]
+  security_groups                 = [module.security-groups.alb_security_group_id]
+  assign_public_ip                = true
+  target_group_arn                = module.application_load_balancer.alb_target_group_arn
+}
