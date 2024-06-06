23ecac86b823836017280b9a8993454a50486d5b
m-pasima <theivykierachronicles@gmail.com>
2024-06-06 02:38:45 +0100

    Update backend.tf

--- terraform-project/backend.tf
+++ terraform-project/backend.tf
@@ -1,7 +1,7 @@
 # This will create s3 bucket to store the terraform state file 
 terraform {
   backend "s3" {
-    bucket    = "terraform-p-backend"
+    bucket    = "your unique bucket"
     key       = "terraform-project.tfstate"
     region    = "eu-west-2"
     profile   = "default"
